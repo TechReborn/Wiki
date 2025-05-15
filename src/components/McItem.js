c2de14013b9d335831509fdbf3ec553241e1bf07
snollygolly <snollygolly@gmail.com>
2025-05-14 19:46:33 -0500

    removing floats from mcitem
    and supporting legacy slug format (pack:id)

--- src/components/McItem.js
+++ src/components/McItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 const GLOBAL = require('../../.docusaurus/globalData.json');
 const PAGES = GLOBAL['docusaurus-plugin-content-docs'].default.versions[0].docs;
 
-export default function McItem({slug, pack, inline = false, size = 24, float = 'left', overrides = {}}) {
+export default function McItem({slug, pack, inline = false, size = 24, overrides = {}}) {
 	// todo: something better here, but if they don't give a slug, what can we even do?
 	if (!slug) { return (''); }
 	// todo: solve for vanilla images, do we want to hotlink or rehost?
@@ -17,14 +17,21 @@ export default function McItem({slug, pack, inline = false, size = 24, float = '
 			return {image, path};
 		}
 	};
+	// support legacy slug id's as well for easy for migration
+	if (slug.indexOf(':') !== -1) {
+		const slugParts = slug.split(':');
+		slug = slugParts[1];
+		pack = slugParts[0];
+	}
 	const friendlyName = overrides.description === false ? '' : titleCase(slug);
 	const items = packMap[pack](slug);
 	const Element = inline === true ? "span" : "div";
 	return (
 		<Element>
 			<strong>
-				<a href={items.path}>{friendlyName}
-					<img alt={friendlyName} style={{float}} width={size} src={items.image}></img>
+				<a href={items.path}>
+					<img alt={friendlyName} width={size} src={items.image}></img>
+					{friendlyName}
 				</a>
 			</strong>
 		</Element>
